212d2914f793c4dc6b08fc6c0a7abdeb5c8d4148
RowanElsworth <rowanelsworth@gmail.com>
2023-03-05 14:37:53 +0000

    event add, deletion, and editation logic created

--- pages/events/events.js
+++ pages/events/events.js
@@ -1,14 +1,160 @@
 $(document).ready(function() {
+  // sets up the variable, because I can't think of a better way of doing it lol
+  var eventType;
+  var editIndex;
 
   // load header
   $(function(){
     $("#header").load("../../partials/header.html"); 
   });
-  
+
   // authentication check
   if (!localStorage.getItem('user')) {
     // redirect to login page if not authenticated
     window.location.href = '/pages/auth/auth.html';
   }
+
+  // Out puts the events in alphabetical order to the screen
+  let events = JSON.parse(localStorage.getItem('events'))
+  // Sort the events by name
+  events.sort((a, b) => a.name.localeCompare(b.name));
+
+  // Loop through the events and add them to the events table
+  events.forEach(function(event) {
+    // Determine the type of the event (team or individual)
+    var eventTypeClass = event.type === "Team" ? "team" : "individual";
   
+    // Create a new row for the event
+    var newRow = $("<div>").addClass("row");
+  
+    // Create the cols div for the event
+    var colsDiv = $("<div>").addClass("cols");
+    
+    // Create the left column for the event name
+    var colLeft = $("<div>").addClass("col-left");
+    var eventName = $("<div>").addClass("event-name");
+    eventName.append($("<p>").text(event.name));
+    colLeft.append(eventName);
+    colsDiv.append(colLeft);
+  
+    // Create the right column for the start time and edit button
+    var colRight = $("<div>").addClass("col-right");
+    var startControls = $("<div>").addClass("event-start-controls");
+    startControls.append($("<p>").text(event.startTime));
+    startControls.append($("<button>").addClass("edit-btn").text("Edit").attr("data-index", event.index));
+    colRight.append(startControls);
+    colsDiv.append(colRight);
+  
+    // Add the cols div to the row
+    newRow.append(colsDiv);
+  
+    // Add the new row to the events table
+    $(".events." + eventTypeClass + " .table-container").append(newRow);
+  });
+
+  // ~~~ Add Event ~~~
+  // add event pop up box
+  $(".add-btn").click(function() {
+    $(".add-event-popup-container").show();
+    eventType = $(this).attr('data-type');
+  });
+  $(".close-btn").click(function() {
+    $(".add-event-popup-container").hide();
+  });
+
+  $(".submit-btn").click(function(event) {
+    event.preventDefault();
+
+    // Get the event name and start time from the input fields
+    var eventName = $("#name").val();
+    var startTime = $("#start-time").val();
+
+    // Determine the next index for the new event
+    var events = JSON.parse(localStorage.getItem('events'));
+    var index = events.length + 1;
+
+    // Create a new event object
+    var newEvent = {
+      index: index,
+      type: eventType,
+      name: eventName,
+      startTime: startTime,
+      participants: []
+    };
+    console.log(newEvent)
+
+    // Push to local storage
+    if (localStorage.getItem('events') == undefined) {
+      localStorage.setItem('events', "[]");
+      events.push(newEvent)
+      localStorage.setItem('events', JSON.stringify(events));
+      location.reload();
+    } else {
+      events.push(newEvent)
+      localStorage.setItem('events', JSON.stringify(events));
+      location.reload();
+    }
+  });
+
+  // ~~~ Edit Event ~~~
+  // Edit button click event handler
+  $(".edit-btn").click(function() {
+    $(".edit-event-popup-container").show();
+    // Get the index of the event to edit from the data-index attribute of the clicked button
+    editIndex = $(this).attr("data-index");
+    console.log(editIndex);
+    // Get the event data from local storage
+    var events = JSON.parse(localStorage.getItem('events'));
+    var event = events.find(function(item) {
+      return item.index === parseInt(editIndex);
+    });
+    // Set the values of the edit-event-popup fields to the event data
+    $(".edit-event-type").text(event.type);
+    $(".edit-event-name").val(event.name);
+    $(".edit-event-time").val(event.startTime);
+  });
+
+  $(".close-btn").click(function() {
+    $(".edit-event-popup-container").hide();
+  });
+
+  // Confirm button click event handler
+  $(".confirm-btn").click(function() {
+    // Get the edited event data from the edit-event-popup fields
+    var eventName = $(".edit-event-name").val();
+    var startTime = $(".edit-event-time").val();
+    // Get the events from local storage
+    var events = JSON.parse(localStorage.getItem('events'));
+    // Find the event in the events array with the matching index value
+    var eventIndex = events.findIndex(function(item) {
+      return item.index === parseInt(editIndex);
+    });
+    // Update the event data in the events array
+    events[eventIndex].name = eventName;
+    events[eventIndex].startTime = startTime;
+    // Update the events array in local storage
+    localStorage.setItem('events', JSON.stringify(events));
+    // Hide the edit-event-popup
+    $(".edit-event-popup-container").hide();
+    // Reload the page
+    location.reload();
+  });
+  
+  // Delete button click event handler
+  $(".delete-btn").click(function() {
+    editIndex = parseInt(editIndex); 
+    // Get the events from local storage
+    var events = JSON.parse(localStorage.getItem('events'));
+    // Filter the events array to remove the event with the matching index value
+    events = events.filter(function(item) {
+      return item.index !== editIndex;
+    });
+    // Update the events array in local storage
+    localStorage.setItem('events', JSON.stringify(events));
+    // Hide the edit-event-popup
+    $(".edit-event-popup-container").hide();
+    // Reload the page
+    location.reload();
+  });
+
 });
--- pages/schedule/schedule.js
+++ pages/schedule/schedule.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  updateNextEvent();
+  updateEventList();
 
   // load header
   $(function(){
@@ -11,40 +13,83 @@ $(document).ready(function() {
     window.location.href = '/pages/auth/auth.html';
   }
   
-
-  
-  // updates next event
-  $.getJSON('../../events.json', function(data) {
-    var sortedEvents = data.events.sort(function(a, b) {
-      var timeA = new Date('1970/01/01 ' + a.startTime);
-      var timeB = new Date('1970/01/01 ' + b.startTime);
-      return timeA - timeB;
-    });
-    var soonestEvent = sortedEvents[0];
-    
-    var output = {
-      "type": soonestEvent.type,
-      "name": soonestEvent.name,
-      "startTime": soonestEvent.startTime,
-      "participants": soonestEvent.participants
+  function updateNextEvent() {
+    let events = JSON.parse(localStorage.getItem('events'))
+      var currentTime = new Date();
+      var nextEvent = null;
+      var nextEventTime = null;
+      
+      // Loop through all events
+      $.each(events, function(index, event) {
+        var eventTime = new Date();
+        var timeParts = event.startTime.split(':');
+        eventTime.setHours(timeParts[0]);
+        eventTime.setMinutes(timeParts[1]);
+        
+        // If event is in the future and sooner than the current next event, set it as the new next event
+        if (eventTime > currentTime && (nextEventTime == null || eventTime < nextEventTime)) {
+          nextEvent = event;
+          nextEventTime = eventTime;
+        }
+      });
+      
+      // Display the name of the next event 
+      $('.event-type').html(`${nextEvent.type}`);
+      $('.event-name').html(`<span>${nextEvent.name}</span> @ ${nextEvent.startTime}`);
+      // Append participants of next event one by one to the upcoming-events-list
+      var participantsList = '';
+      $.each(nextEvent.participants, function(index, participant) {
+        participantsList += `<p>${participant}</p>`;
+      });
+      $('.event-participants').html(participantsList);
     };
-    console.log(output);
-  });
+  // Call the updateNextEvent function every minute
+  setInterval(updateNextEvent, 60000);
   
-
-
+  function updateEventList() {
+      let events = JSON.parse(localStorage.getItem('events'))
+      var currentTime = new Date();
+      var upcomingEvents = [];
   
-
-  $.getJSON("../../events.json", function(data) {
-    var events = data.events.concat(data.indEvents);
-    events.sort(function(a, b) {
-      return new Date("1970/01/01 " + a.startTime) - new Date("1970/01/01 " + b.startTime);
-    });
-    $.each(events, function(index, event) {
-      var html = `<p>${event.type} <span>${event.name}</span><span>${event.startTime} <a href="">View</a></span></p>`;
-      $(".upcoming-events-list").append(html);
-    });
-  });
+      // Loop through all events
+      $.each(events, function(index, event) {
+        var eventTime = new Date();
+        // Split the start time string into hours and minutes
+        var timeParts = event.startTime.split(':');
+        // Set the hours and minutes of the event time object
+        eventTime.setHours(timeParts[0]);
+        eventTime.setMinutes(timeParts[1]);
   
+        // Check if the event is in the future
+        if (eventTime > currentTime) {
+          upcomingEvents.push(event);
+        }
+      });
+  
+      // Sort the upcoming events array by start time in ascending order
+      upcomingEvents.sort(function(a, b) {
+        var timePartsA = a.startTime.split(':');
+        var timePartsB = b.startTime.split(':');
+        var timeA = new Date();
+        var timeB = new Date();
+        timeA.setHours(timePartsA[0]);
+        timeA.setMinutes(timePartsA[1]);
+        timeB.setHours(timePartsB[0]);
+        timeB.setMinutes(timePartsB[1]);
+        return timeA - timeB;
+      });
+  
+      // Clear the existing event list
+      $('.upcoming-events-list').empty();
+  
+      // Loop through all upcoming events and add them to the event list
+      $.each(upcomingEvents, function(index, event) {
+        var eventHtml = `<p><span>${event.type}</span> ${event.name} <span>${event.startTime}</span></p>`;
+        $('.upcoming-events-list').append(eventHtml);
+      });
+    };
+  // Call the updateUpcomingEvents function every minute
+  setInterval(updateEventList, 60000);
+    
 });
   
